11acf5e1ecd628425f7067916588bf29b7e09c1d
Kresky <kresky@protonmail.com>
2023-12-04 10:51:59 +0100

    add drop schema if exist

--- backendAPI-data.sql
+++ backendAPI-data.sql
@@ -14,6 +14,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema cafe_finder
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cafe_finder` ;
 CREATE SCHEMA IF NOT EXISTS `cafe_finder` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `cafe_finder` ;
 
