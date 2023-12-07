0cfe27c09a3f3122735f4b95a258b5c6bbf4e1f3
ViktorBach <vmb0107@gmail.com>
2023-12-07 13:31:20 +0100

    filter update

--- cafelist.js
+++ cafelist.js
@@ -1,5 +1,5 @@
-const filters = document.querySelectorAll('select')
-const cafelist = document.querySelector(".cafeList")
+const filters = document.querySelectorAll('select');
+const cafelist = document.querySelector(".cafeList");
 filters.forEach(filter => {
     filter.addEventListener('change', function() {
         const open = document.querySelector("#openingHours").value
@@ -9,16 +9,38 @@ filters.forEach(filter => {
         const wifi = document.querySelector("#wifi").value
         const apiUrl = `http://localhost:3000/listfilter?opening_hours=${open}&closing_hours=${close}&city=${location}&price_range=${price}&wifi=${wifi}`;
         console.log(apiUrl);
-        fetch(`http://localhost:3000/listfilter?opening_hours=${open}&closing_hours=${close}&city=${location}&price_range=${price}&wifi=${wifi}`,
-            { method: 'GET' })
+        fetch(apiUrl,{ method: 'GET' })
             .then(response => response.json())
             .then(data => {
                 if (data.length > 0) {
                     console.log(data);
                 } else {
-                    console.log("no cafe returned")
+                    console.log("no cafe returned");
                 }
 
-            })
-    })
-})
+                cafelist.innerHTML = '';
+
+                if (data.length > 0) {
+                    const firstCafe = data[0]
+                    const cafeInfoElement = document.createElement('div');
+                    cafeInfoElement.innerHTML = `
+                    <p>Opening Hours: ${firstCafe.opening_hours}</p>
+                    <p>Closing Hours: ${firstCafe.closing_hours}</p>
+                    <p>City: ${firstCafe.city}</p>
+                    <p>Address: ${firstCafe.address}</p>
+                    <p>Price Range: ${firstCafe.price_range}</p>
+                    <p>Wi-Fi: ${firstCafe.wifi}</p>
+                    <p>Info: ${firstCafe.info}</p>
+                  `;
+
+                    cafelist.appendChild(cafeInfoElement);
+                } else {
+                    console.log("No cafe returned");
+                }
+
+        })
+            .catch(error => {
+                console.log("error ", error);
+            });
+    });
+});
